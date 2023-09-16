d3f25f0f1e33007b134cfe0ef2ec4351e9bb3c65
MazharSolkar <mazharsolkar65@gmail.com>
2023-09-16 17:30:54 +0530

    Main container video background part complete

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -9,12 +9,12 @@ const MainContainer = () => {
   // if (movies === null) return; if movies not present return from here below is same
   if (!movies) return;
 
-  const mainMovie = movies[0];
+  const mainMovie = movies[4];
 
   const { original_title, overview, id } = mainMovie;
 
   return (
-    <div>
+    <div className=''>
       <VideoTitle title={original_title} overview={overview} />
       <VideoBackground movieId={id} />
     </div>
--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,14 +1,14 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className='pt-36 px-12'>
+    <div className='absolute w-screen aspect-video pt-[20%] px-24  text-white bg-gradient-to-r from-black'>
       <h1 className='text-6xl font-bold'>{title}</h1>
       <p className='py-6 text-lg w-1/4'>{overview}</p>
       <div className=''>
-        <button className='bg-gray-500 text-white text-xl p-4 px-12 bg-opacity-80 rounded-lg'>
+        <button className='bg-white text-black text-xl p-4 px-12  rounded-lg hover:bg-opacity-80'>
           ▶ Play
         </button>
-        <button className='mx-2 bg-gray-500 text-white text-xl p-4 px-12 bg-opacity-80 rounded-lg'>
-          More Info
+        <button className='mx-2 bg-gray-700 opacity-50 text-white text-xl p-4 px-12 rounded-lg hover:bg-opacity-80'>
+          ℹ️ More Info
         </button>
       </div>
     </div>
